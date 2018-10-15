dbfb4318c37e372e480f8f216d279a985a566498
Fei Gao <fgao1@ualberta.ca>
2018-10-15 13:55:31 -0600

    validate password minimum length

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
     validates :email, presence: true, uniqueness: {case_sensitive: false}
     validates :password, presence: true
     validates :password_confirmation, presence: true
+    validates_length_of :password, minimum: 6
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject {
     User.create(name: 'Jone Doe', email: 'jonedoe@gmail.com',
-      password:'123', password_confirmation: '123')
+      password:'123456', password_confirmation: '123456')
     }
     
     describe 'Validations' do
@@ -12,6 +12,7 @@ RSpec.describe User, type: :model do
         expect(subject).to be_valid
       end
     
+      # validate fields cannot be empty
       it "is not valid without a name" do
         subject.name = nil
         expect(subject).to_not be_valid
@@ -27,16 +28,23 @@ RSpec.describe User, type: :model do
         expect(subject).to_not be_valid
       end
 
+      # validate matching password
       it "is not valid without a matching password" do
         user1 = User.create(name: 'James', email: 'james@gmail.com', password: 'james123', password_confirmation: 'jam123')
         expect(user1).to_not be_valid
       end
 
+      # validate unique email
       it "is not valid without an unique email" do
-        User.create(name: 'Jone Doe', email: 'jonedoe@gmail.com', password: '123', password_confirmation: '123') 
-        user1 = User.new(name: 'Jone Doe', email: 'JONEDOE@gmail.com', password: '123', password_confirmation: '123')      
+        User.create(name: 'Jone Doe', email: 'jonedoe@gmail.com', password: '123456', password_confirmation: '123456') 
+        user1 = User.new(name: 'Jone Doe', email: 'JONEDOE@gmail.com', password: '123456', password_confirmation: '123456')      
         expect(user1).to_not be_valid
       end
 
+      # validate password minimum length
+      it "is not valid if password length less than 6" do
+      user1 = User.new(name: 'Jane Doe', email: 'janedoe@gmail.com', password: '123', password_confirmation: '123')      
+      expect(user1).to_not be_valid
+      end
     end
 end
