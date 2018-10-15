8e7721216dec10de856d6fed06586ee5f3f73183
Fei Gao <fgao1@ualberta.ca>
2018-10-15 13:17:21 -0600

    define validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :reviews
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: {case_sensitive: false}
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
 end
